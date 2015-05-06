6fa71cd7ad0a6b367e4f3c82d999d959ddcfbbbc
CCoffie <CalebCoffie@gmail.com>
2015-05-06 00:50:36 -0400

    Finishing up configuration creation. Including example module requirements and generated configs.

--- src/2.7/Web/app/main/forms.py
+++ src/2.7/Web/app/main/forms.py
@@ -1,11 +1,60 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
-    SubmitField
+    SubmitField, RadioField
 from wtforms.validators import Required, Length, Email, Regexp
 from wtforms import ValidationError
+import os, json
 
 class GenerateConfiguration(Form):
-    name = StringField('Real name', validators=[Length(0, 64)])
-    location = StringField('Location', validators=[Length(0, 64)])
-    about_me = TextAreaField('About me')
+    configurations = {}
+
+    # Get Modules Folder
+    modules_folder = os.path.dirname(os.getcwd())
+    modules_folder = os.path.join(modules_folder, "Modules")
+
+    # Load configurations from these files
+    for file in os.listdir(modules_folder):
+        if file.endswith(".json"):
+            with open(os.path.join(modules_folder, file)) as data_file:
+                configurations[file] = json.load(data_file)
+
+    for moduleName, configuration in configurations.iteritems():
+        friendlyModuleName = moduleName.split("_")[0]
+
+        # Module Enable button
+        exec(friendlyModuleName + "_Enabled" + " = BooleanField('Enable " +
+            friendlyModuleName +"')")
+
+        for field in configuration["params"]:
+            friendlyFieldName = field["name"].replace(" ", "_")
+
+            # Boolean Field
+            if field["fieldType"] == "BooleanField":
+                exec(friendlyModuleName + "_" + friendlyFieldName + " = BooleanField('" +
+                    field["name"] + "', description='" + field["description"] +
+                    "')")
+
+            # String Field
+            if field["fieldType"] == "StringField":
+                exec(friendlyModuleName + "_" + friendlyFieldName + " = StringField('" +
+                    field["name"] + "', description='" + field["description"] +
+                    "')")
+
+            # Dropdown Field
+            if field["fieldType"] == "SelectField":
+                exec(friendlyModuleName + "_" + friendlyFieldName + " = SelectField('" +
+                    field["name"] + "', description='" + field["description"] +
+                    "', choices=" + str([tuple(choice) for choice in field["options"]]) + ")")
+
+            # Radio Button Field
+            if field["fieldType"] == "RadioField":
+                exec(friendlyModuleName + "_" + friendlyFieldName + " = RadioField('" +
+                    field["name"] + "', default=" + str(field["options"][0][0]) + ", description='" + field["description"] +
+                    "', choices=" + str([tuple(choice) for choice in field["options"]]) + ")")
+
+            # This will not work currently with wtf.quick_form
+            # Label Field
+            # if field["fieldType"] == "Label":
+            #     exec(friendlyModuleName + "_" + friendlyFieldName +
+            #         " = Field("label='" + field["name"] + "')")
     submit = SubmitField('Submit')
--- src/2.7/Web/app/main/views.py
+++ src/2.7/Web/app/main/views.py
@@ -2,10 +2,47 @@ from flask import render_template, redirect, url_for, abort, flash, request,\
     current_app, make_response
 from . import main
 from .forms import GenerateConfiguration
+import os, json
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = GenerateConfiguration()
     if form.validate_on_submit():
-        print("Hey look at that!")
+        # List of Modules
+        modules = []
+
+        # Get Modules Folder
+        modules_folder = os.path.dirname(os.getcwd())
+        modules_folder = os.path.join(modules_folder, "Modules")
+
+        # Load names of modules into variable
+        for file in os.listdir(modules_folder):
+            if file.endswith(".json"):
+                modules.append(file.split("_")[0])
+
+        enabledModules = []
+
+        # Look for Enabled Modules
+        enabled = False
+        for module in modules:
+            exec("enabled = form." + module + "_Enabled.data")
+            if enabled:
+                enabledModules.append(module)
+
+        for enabledModule in enabledModules:
+            moduleOptions = {}
+
+            # loop through form data
+            for key in dir(form):
+                if key.split("_")[0] == enabledModule:
+                    moduleLength = len(enabledModule) + 1
+                    exec("moduleOptions['" + key[moduleLength:] + "'] = form." + key + ".data")
+
+            # Output configuration file
+            jsonObject = json.dumps(moduleOptions, indent=4)
+
+            file = open(os.path.join(modules_folder, enabledModule + "_config.json"), 'w')
+            file.write(jsonObject)
+            file.close()
+            
     return render_template('index.html', form=form)
